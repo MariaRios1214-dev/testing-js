15896648e48844b5830bf7d60486204e8953dead
Maria Fernanda Rios Cardenas <ingfernandarios1214@gmail.com>
2023-04-10 06:12:37 -0500

    Generando fakes de forma dinamica con Fake

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,22 +1,12 @@
 const BookService = require('./books.service');
+const { generateManyBooks } = require('../fakes/book.fake');
 
-const fakeBooks = [
-  {
-    _id: 1,
-    name: 'Harry Potter',
-  },
-  {
-    _id: 2,
-    name: 'Mente Millonaria',
-  },
-];
+const mockGetAll = jest.fn();
 
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
   create: () => {},
-};
-
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+})));
 
 describe('Test for BooskService', () => {
   let service;
@@ -27,12 +17,28 @@ describe('Test for BooskService', () => {
 
   describe('test for getBooks', () => {
     test('should return a list books', async () => {
+      const fakeBooks = generateManyBooks(20);
       // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
-      const books = await service.getBooks();
+      const books = await service.getBooks({});
       console.log('books', books);
       // Assert
-      expect(books.length).toBe(2);
+      expect(books.length).toEqual(20);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+    });
+    test('should return a list books, equal first data', async () => {
+      const fakeBooks = generateManyBooks(4);
+      // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      const books = await service.getBooks({});
+      console.log('books', books);
+      // Assert
+      expect(books.length).toEqual(4);
+      expect(books[0].name).toEqual(fakeBooks[0].name);
     });
   });
 });
